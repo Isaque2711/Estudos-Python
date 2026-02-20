72cbc888498a0e367a4f99945bfcde9d5980e039
Isaque2711 <isaquerodrigues7535@gmail.com>
2026-02-20 14:28:32 -0400

    Add multiplication and division to calculator

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,32 @@
-cal = float(input('Digite [1] para soma e [2] para subtração: '))
+cal = float(input('Digite [1] para soma, [2] para subtração, [3] para multiplicação e [4] para divisão: '))
 def soma(a,b):
     return a + b
 
 def sub(a,b):
     return a - b
 
+def mult(a,b):
+    return a * b
+
+def div(a,b):
+    return a / b
+
 def main():
     a = float(input('Digite um valor: '))
     b = float(input('Digite outro valor: '))
-    s = soma(a,b)
-    su = sub(a,b)
-    if cal==1:
-        print(s)
-    elif cal==2:
-        print(su)
+    res_soma = soma(a, b)
+    res_sub = sub(a, b)
+    res_mult = mult(a, b)
+    res_div = div(a, b)
+
+    if cal == 1:
+        print(res_soma)
+    elif cal == 2:
+        print(res_sub)
+    elif cal == 3:
+        print(res_mult)
+    elif cal == 4:
+        print(res_div)
     else:
         print("Opreação inválida!")
 main()
